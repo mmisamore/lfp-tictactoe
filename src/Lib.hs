3fc9ef6f3959387b4d52eb9de3258ca3542a8c3a
Michael D. Misamore <m.misamore@gmail.com>
2017-09-11 21:29:36 -0400

    More experiments with equality types

--- src/Lib.hs
+++ src/Lib.hs
@@ -18,24 +18,12 @@ data Coord = First | Second | Third
 -- The complete board state as a list of plays
 data BoardRep = Empty | Cons Mark Coord Coord BoardRep
 
--- Extreme case: *all* information about board state is at the type level
+-- Type-level game board 
 data Board (rep :: BoardRep) = Board
 
 -- An empty game board
 newBoard :: Board 'Empty
-newBoard = Board
-
--- Given a board at the type level, determine who moves next 
-type family NextTurn (rep :: BoardRep) :: Mark where
-  NextTurn 'Empty           = 'X
-  NextTurn ('Cons 'X _ _ _) = 'O
-  NextTurn ('Cons 'O _ _ _) = 'X
-
--- Given a board at the type level, determine if a position has been played
-type family Played (row :: Coord) (col :: Coord) (rep :: BoardRep) :: Bool where
-  Played _ _ 'Empty                     = 'False
-  Played row col ('Cons _ row col prev) = 'True
-  Played row col ('Cons _ _ _ prev)     = Played row col prev
+newBoard = Board 
 
 -- Given a board at the type level, get the mark at a position
 type family BoardVal (row :: Coord) (col :: Coord) (rep :: BoardRep) :: Symbol where
@@ -61,7 +49,7 @@ type family ShowBoard (rep :: BoardRep) :: Symbol where
 
 -- Helper function: convert a Board to a type-level string
 showBoard :: Board (rep :: BoardRep) -> Proxy (ShowBoard rep) 
-showBoard Board = Proxy 
+showBoard _ = Proxy 
 
 -- Convert an empty game board (at type level) to a string 
 showEmptyBoard :: String
@@ -73,36 +61,68 @@ data CoordL (coord :: Coord) where
   Second' :: CoordL 'Second
   Third'  :: CoordL 'Third
 
--- Make a play on the board if player X is next and position is available
-xPlay :: (NextTurn rep ~ 'X, Played row col rep ~ 'False) => 
-  CoordL row 
+-- Lower CoordL down to term level
+coordVal :: CoordL (coord :: Coord) -> Coord
+coordVal First'  = First
+coordVal Second' = Second 
+coordVal Third'  = Third
+
+-- Witness for type equality
+data Equals a b where
+  Refl :: Equals a a
+
+-- Type equality is reflexive
+refl :: Equals a a
+refl = Refl
+
+-- Type equality is symmetric
+sym :: Equals a b -> Equals b a
+sym Refl = Refl
+
+-- Type equality is transitive
+trans :: Equals b c -> Equals a b -> Equals a c
+trans Refl Refl = Refl
+
+-- Flipping marks
+type family Flip (a :: Mark) :: Mark where
+  Flip 'X = 'O
+  Flip 'O = 'X
+
+-- Given a board at the type level, determine who moves next 
+type family NextTurn (rep :: BoardRep) :: Mark where
+  NextTurn 'Empty          = 'X
+  NextTurn ('Cons a _ _ _) = Flip a 
+
+-- Given a board at the type level, determine if a position has been played
+type family Played (row :: Coord) (col :: Coord) (rep :: BoardRep) :: Bool where
+  Played _ _ 'Empty                     = 'False
+  Played row col ('Cons _ row col prev) = 'True
+  Played row col ('Cons _ _ _ prev)     = Played row col prev
+
+-- Reify constraints as equality types, chain along the next constraint 
+play :: 
+  Equals (NextTurn rep) a
+  -> CoordL row 
   -> CoordL col 
   -> Board (rep :: BoardRep) 
-  -> Board ('Cons 'X row col rep) 
-xPlay _ _ _ = Board 
+  -> (Board ('Cons a row col rep), Equals (NextTurn ('Cons a row col rep)) (Flip a)) 
+play Refl row col Board = (Board, Refl)
 
--- Make a play on the board if player O is next and position is available
-oPlay :: (NextTurn rep ~ 'O, Played row col rep ~ 'False) => 
-  CoordL row 
-  -> CoordL col 
-  -> Board (rep :: BoardRep) 
-  -> Board ('Cons 'O row col rep) 
-oPlay _ _ _ = Board 
+-- Another constraint: Played row col rep ~ 'False 
 
--- Examples:
--- symbolVal (showBoard (xPlay First' First' newBoard))
--- symbolVal (showBoard (oPlay First' First' newBoard))                          -- type error: X always plays first
--- symbolVal (showBoard (xPlay First' First' (xPlay Third' Second' newBoard)))   -- type error: X played twice
--- symbolVal (showBoard (oPlay First' Second' (xPlay First' Second' newBoard)))  -- type error: position already taken
--- symbolVal (showBoard (oPlay Second' Second' (xPlay First' Second' newBoard))) -- successful move 
+-- Play Examples:
+experiment1 = play Refl First' First' newBoard
+experiment2 = let (board1, eq1) = experiment1
+              in play eq1 Second' Second' board1
+experiment3 = let (board2, eq2) = experiment2
+              in play eq2 Third' Third' board2
 
 -- What we have so far:
 -- Each board location must be one of: X, O, or E
 -- Safe indexing into the board via coordinates
 -- Enforce who moves first
 -- Turns alternate between X and O players 
--- Protection from repeated plays or overwrites of previous moves
 
 -- What we don't have:
--- More turns not allowed even if a player has already won
-
+-- Protection from repeated plays or overwrites of previous moves
+-- An implementation that computes required types at runtime based on user input (dependent types)
